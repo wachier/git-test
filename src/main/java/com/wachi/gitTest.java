32c3c21f44d4361a74fef8b316d349b587fd5cb1
wachier <wachier@yier.com>
2022-12-06 22:52:07 +0800

    first commit

---
+++ src/main/java/com/wachi/gitTest.java
@@ -0,0 +1,11 @@
+package com.wachi;
+
+/**
+ * @version 1.0
+ * @auther …^_^… wa池儿
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
