c4c73b7be42fd012d8db6f423ba48fed94bc82e6
wachier <wachier@yier.com>
2022-12-06 22:55:06 +0800

    second commit

--- src/main/java/com/wachi/gitTest.java
+++ src/main/java/com/wachi/gitTest.java
@@ -7,5 +7,6 @@ package com.wachi;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello waCheer");
     }
 }
