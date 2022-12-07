b7ed68572cbcaeb4ea4c2bcabd3a2b0b249ebae4
wachier <wachier@yier.com>
2022-12-07 10:31:02 +0800

    hot-fix commit

--- src/main/java/com/wachi/gitTest.java
+++ src/main/java/com/wachi/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("hello git!");
         System.out.println("hello waCheer");
         System.out.println("hello kaKaXi");
+        System.out.println("brouhaha");
     }
 }
